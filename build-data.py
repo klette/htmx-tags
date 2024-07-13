9d44902447ec5a59bfabc9ef49d69cee3f14a5c8
Saeid Javadi <S.Javadi.P@gmail.com>
2024-07-13 08:40:45 +0330

    update document and support django-html

--- build-data.py
+++ build-data.py
@@ -7,53 +7,25 @@ from pathlib import Path
 import json
 
 result = {
-    "version": 1.1,
-    "tags": [],
+    "version": 1.1, "tags": [],
     "globalAttributes": [],
-    "valueSets": [
-        {
-            "name": "swap",
-            "values": [
-                {
-                    "name": "innerHTML",
-                    "description": "The default, puts the content inside the target element",
-                },
-                {
-                    "name": "outerHTML",
-                    "description": "Replaces the entire target element with the returned content",
-                },
-                {
-                    "name": "afterbegin",
-                    "description": "Prepends the content before the first child inside the target",
-                },
-                {
-                    "name": "beforebegin",
-                    "description": "Prepends the content before the target in the targets parent element",
-                },
-                {
-                    "name": "beforeend",
-                    "description": "Appends the content after the last child inside the target",
-                },
-                {
-                    "name": "afterend",
-                    "description": "Appends the content after the target in the targets parent element",
-                },
-                {
-                    "name": "delete",
-                    "description": "Deletes the target element regardless of the response",
-                },
-                {
-                    "name": "none",
-                    "description": "Does not append content from response (Out of Band Swaps and Response Headers will still be processed)",
-                },
-            ],
-        }
-    ],
-}
+    "valueSets":
+    [{"name": "swap",
+      "values":
+      [{"name": "innerHTML", "description": "The default, puts the content inside the target element", },
+       {"name": "outerHTML", "description": "Replaces the entire target element with the returned content", },
+       {"name": "afterbegin", "description": "Prepends the content before the first child inside the target", },
+       {"name": "beforebegin", "description": "Prepends the content before the target in the targets parent element", },
+       {"name": "beforeend", "description": "Appends the content after the last child inside the target", },
+       {"name": "afterend", "description": "Appends the content after the target in the targets parent element", },
+       {"name": "delete", "description": "Deletes the target element regardless of the response", },
+       {"name": "none",
+        "description":
+        "Does not append content from response (Out of Band Swaps and Response Headers will still be processed)", },], }], }
 
 documented_value_sets = {e["name"] for e in result["valueSets"]}
 
-HTMX_VERSION = "1.9.6"
+HTMX_VERSION = "2.0.0"
 
 response = urlopen(
     f"https://github.com/bigskysoftware/htmx/archive/refs/tags/v{HTMX_VERSION}.zip"
